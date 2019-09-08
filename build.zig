55897ca0e0483168568533ee57938401365ada62
Rickard Andersson <gonz@severnatazvezda.com>
2019-09-09 00:19:25 +0300

    clean up build script
    also remove `gui` file

--- build.zig
+++ build.zig
@@ -4,15 +4,7 @@ pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("disk-info", "src/main.zig");
     exe.addPackagePath("win32", "./dependencies/zig-win32/src/main.zig");
-    // exe.addIncludeDir("C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/um");
-    // exe.addIncludeDir("C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/ucrt");
-    // exe.addIncludeDir("C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/winrt");
-    // exe.addIncludeDir("C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/shared");
-    // exe.addIncludeDir("C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/include");
     exe.linkSystemLibrary("c");
-    // exe.linkSystemLibrary("gdi32");
-    // exe.linkSystemLibrary("user32");
-    // exe.linkSystemLibrary("kernel32");
     exe.setBuildMode(mode);
     exe.install();
 
