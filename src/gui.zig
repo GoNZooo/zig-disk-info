26230060cfdb0b26d39d7b7f5dfe308e3cd0eb74
Rickard Andersson <gonz@severnatazvezda.com>
2020-04-01 23:25:16 +0300

    add primary & secondary IDs

--- src/gui.zig
+++ src/gui.zig
@@ -1,8 +1,18 @@
 const std = @import("std");
 
 const State = struct {
-    hot: Id,
-    active: Id,
+    hot: ?Id,
+    active: ?Id,
 };
 
-const Id = []const u8;
+const Id = struct {
+    primary: PrimaryId,
+    secondary: SecondaryId = 0,
+
+    pub fn isEqual(self: Id, id: Id) bool {
+        return mem.eql(u8, self.primary, id.primary) and self.secondary == id.secondary;
+    }
+};
+
+const PrimaryId = []const u8;
+const SecondaryId = u32;
