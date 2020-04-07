30fd36e4f2364d8527fea7383880394a2c3ba071
Rickard Andersson <gonz@severnatazvezda.com>
2020-04-08 00:19:03 +0300

    fix high word usage in `bitblit_main.zig`

--- src/bitblit_main.zig
+++ src/bitblit_main.zig
@@ -120,7 +120,7 @@ export fn windowProcedure(
         },
         win32.c.WM_LBUTTONUP => {
             const x = win32.lowWord(win32.c.LPARAM, lParam);
-            const y = win32.highWord(win32.c.LPARAM, lParam & 0xffff0000) >> 16;
+            const y = win32.highWord(win32.c.LPARAM, lParam);
             const current_x: c_int = 5;
             var current_y: c_int = 2;
             const height = 20;
